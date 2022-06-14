2fbb80fcefa458ba694817974748e5bae436932f
Enesylcn <86023573+Enesylcn@users.noreply.github.com>
2022-06-14 23:28:58 +0300

    CRUD işlemleri Aktif Hale Getirildi.

--- DataAccessLayer/Concrete/Repositories/GenericRepository.cs
+++ DataAccessLayer/Concrete/Repositories/GenericRepository.cs
@@ -13,7 +13,7 @@ namespace DataAccessLayer.Concrete.Repositories
     {
         Context c = new Context();
         DbSet<T> _object;
-
+         
         public GenericRepository()
         {
             _object = c.Set<T>();
@@ -24,6 +24,11 @@ namespace DataAccessLayer.Concrete.Repositories
             _object.Remove(p);
             c.SaveChanges();
 
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            return _object.SingleOrDefault(filter);
         }
 
         public void Insert(T p)
---
+++ MvcProjectCamp/Controllers/IstatistikController.cs
@@ -0,0 +1,22 @@
+﻿using DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjectCamp.Controllers
+{
+    public class IstatistikController : Controller
+    {
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            ViewBag.Value = c.Categories.Count().ToString();
+            ViewBag.Valu = c.Headings.Include("Yazılım").Where(x => x.CategoryID == 12).Count().ToString();
+            ViewBag.Val = c.Writers.Where(x => x.WriterName.Contains("a")).Count().ToString();
+            return View();
+        }
+    }
+}
